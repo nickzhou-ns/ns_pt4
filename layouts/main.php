f43885591aa5a252c420fdc79993a553d3fb4c87
Nick <nick@newstarsec.com.au>
2024-07-16 17:28:31 +1000

    improve crud

--- layouts/main.php
+++ layouts/main.php
@@ -4,7 +4,7 @@
 function includeFileWithVariables($filePath, $variables = array(), $print = true)
 {
     $output = NULL;
-    if(file_exists($filePath)){
+    if (file_exists($filePath)) {
         // Extract the variables to a local namespace
         extract($variables);
 
@@ -25,22 +25,22 @@ function includeFileWithVariables($filePath, $variables = array(), $print = true
 
 $isScssconverted = false;
 
-require_once ("scssphp/scss.inc.php");
+require_once("../scssphp/scss.inc.php");
 
 use ScssPhp\ScssPhp\Compiler;
 
-if($isScssconverted){
+if ($isScssconverted) {
 
     global $compiler;
     $compiler = new Compiler();
 
-    $compine_css = "assets/css/app.min.css";
+    $compine_css = "../assets/css/app.min.css";
 
-    $source_scss = "assets/scss/config/default/app.scss";
+    $source_scss = "../assets/scss/config/default/app.scss";
 
     $scssContents = file_get_contents($source_scss);
 
-    $import_path = "assets/scss/config/default";
+    $import_path = "../assets/scss/config/default";
     $compiler->addImportPath($import_path);
     $target_css = $compine_css;
 
